76a5ff7efd2b1c779037ab425bc807cb1b904fd1
Konstantin <Sokolov.k007@yandex.ru>
2021-10-11 00:23:02 +0300

    fix: added refetch from useFetchDeskID

--- src/pages/BridgePage.tsx
+++ src/pages/BridgePage.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import React from 'react';
+import React, { useEffect } from 'react';
 import Play from '../components/Play';
 import { useFetchDeskID } from '../hooks/react-query';
 import Spinner from '../components/Spinner';
@@ -10,14 +10,18 @@ const BridgePage = () => {
 	const fetchDeskID = useFetchDeskID();
 	const { remainingCards } = useStore();
 
-	const { isLoading, data: { deck_id } = {} } = fetchDeskID;
+	const { isLoading, refetch, data: { deck_id } = {} } = fetchDeskID;
 
-	const deskID: string | undefined = remainingCards !== null && remainingCards < countCardsInPlay ? 'new' : deck_id;
+	useEffect(() => {
+		if (remainingCards !== null && remainingCards < countCardsInPlay) {
+			refetch();
+		}
+	}, [remainingCards, countCardsInPlay]);
 
 	return isLoading ? (
 		<Spinner />
 	) : (
-		<Play deskID={deskID} />
+		<Play deskID={deck_id} />
 	);
 };
 export default BridgePage;
